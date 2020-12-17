2400763137468c3e405ae7d9097734aade4ff5aa
Fanjin Zhang <zfjsail@163.com>
2020-12-17 15:54:57 +0800

    try sum again

--- model.py
+++ model.py
@@ -174,8 +174,8 @@ class SoftPoolingGATEncoder(GATEncoderGraph):
 
         ego_embs.append(gat_add_tensor[:, 0, :])
 
-        out, _ = torch.max(embedding_tensor, dim=1)
-        # out = torch.sum(embedding_tensor, dim=1)
+        # out, _ = torch.max(embedding_tensor, dim=1)
+        out = torch.sum(embedding_tensor, dim=1)
         out_all.append(out)
         if self.num_aggs == 2:
             out = torch.sum(embedding_tensor, dim=1)
@@ -213,8 +213,8 @@ class SoftPoolingGATEncoder(GATEncoderGraph):
                                                                    self.conv_last_after_pool[i])
 
             # print("emb shape", embedding_tensor.shape)
-            out, _ = torch.max(embedding_tensor, dim=1)
-            # out = torch.sum(embedding_tensor, dim=1)
+            # out, _ = torch.max(embedding_tensor, dim=1)
+            out = torch.sum(embedding_tensor, dim=1)
             out_all.append(out)
             if self.num_aggs == 2:
                 # out = torch.mean(embedding_tensor, dim=1)
