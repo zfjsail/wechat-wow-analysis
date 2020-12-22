8f710bd6b40c43f7da58b1fb3c274886dfb0d575
Fanjin Zhang <zfjsail@163.com>
2020-12-22 14:57:05 +0800

    lr = 0.01 weibo 57+ 1 vs. 0 pooling layer yes!

--- train.py
+++ train.py
@@ -36,7 +36,7 @@ parser.add_argument('--model', type=str, default='diffpool_prone', help="models 
 parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables CUDA training.')
 parser.add_argument('--seed', type=int, default=42, help='Random seed.')
 parser.add_argument('--epochs', type=int, default=300, help='Number of epochs to train.')
-parser.add_argument('--lr', type=float, default=0.1, help='Initial learning rate.')  # wow: 0.01, click: 0.1
+parser.add_argument('--lr', type=float, default=0.01, help='Initial learning rate.')  # wow: 0.01, click: 0.1
 parser.add_argument('--weight-decay', type=float, default=5e-4,
                     help='Weight decay (L2 loss on parameters).')
 parser.add_argument('--dropout', type=float, default=0.5,
